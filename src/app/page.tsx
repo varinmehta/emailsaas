6337e6e72c48756345d3f7ce73c817da56503b33
varinmehta <mehtavarin2004@gmail.com>
2024-11-16 18:28:29 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,5 @@ import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 
 export default async function Home() {
-  return <h1>Hello, Varin </h1>
+  return <h1>Hello, Varin Mehta</h1>
 }
