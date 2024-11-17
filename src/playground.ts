0a50d8bfc045e1198854c52635863b2d8fd44288
varinmehta <mehtavarin2004@gmail.com>
2024-11-17 09:00:10 +0000

    Nov 17, 2024, 2:30 PM

---
+++ src/playground.ts
@@ -0,0 +1,12 @@
+import { db } from "./server/db";
+
+await db.user.create({
+    data: {
+        emailAddress: 'varin@test.com',
+        firstName: 'Varin',
+        lastName: 'Mehta',
+        imageUrl: 'http://github.com/varinmehta'
+    }
+})
+
+console.log('done')
