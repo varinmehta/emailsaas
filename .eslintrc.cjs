61736bb4f3527a70a159b11487834f29cee0fca6
varinmehta <mehtavarin2004@gmail.com>
2024-11-17 09:47:16 +0000

    11/17/2024, 3:17 PM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,18 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
+  plugins: ["@typescript-eslint"],
+  extends: [
     "next/core-web-vitals",
     // "plugin:@typescript-eslint/recommended-type-checked",
     // "plugin:@typescript-eslint/stylistic-type-checked"
   ],
-  "rules": {
+  rules: {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-  }
-}
+  },
+};
 module.exports = config;
