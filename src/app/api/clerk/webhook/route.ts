a4098e155c0dce90f8d287fde493f19ebecbdd6a
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 04:51:05 +0000

    11/18/2024, 10:21 AM

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,6 +1,6 @@
 export const POST = async (req: Request) => {
     const { data } = await req.json()
     console.log('clerk webhook received', data)
-    const emailAddress = data.email
+    const emailAddress = data.email_addresses[]
     return new Response('Webhook received', { status: 200 })
 }
