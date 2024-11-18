dcc1fb68fbef107e73cf3a4620cf65e39ef12f05
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 06:16:20 +0000

    11/18/2024, 11:46 AM

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -7,5 +7,6 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
         clientId: process.env.AURINKO_CLIENT_ID as string,
         serviceType,
         scopes: 'Mail.Read Mail.ReadWrite Mail.Send Mail.Drafts Mail.All',
+        responseType: 'code'
     })
 }
