071a2797b0d25979b2774fdf8d216102bf08256e
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 05:04:40 +0000

    11/18/2024, 10:34 AM

--- next.config.js
+++ next.config.js
@@ -6,12 +6,12 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default config;
