02c9a1ded901bc79a6815ca3de83f30aae1f389b
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 06:13:57 +0000

    11/18/2024, 11:43 AM

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -2,5 +2,5 @@
 import { auth } from "@clerk/nextjs/server"
 export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
     const { userId } = await auth()
-    if(!userId) throw new Error("Unauthorized user")
+    if(!userId) throw new Error("Unauthorized")
 }
