9860a6e88a68adc4f857505fa9cf64c86aaab5d0
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 06:15:36 +0000

    11/18/2024, 11:45 AM

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -4,6 +4,8 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
     const { userId } = await auth()
     if (!userId) throw new Error("Unauthorized")
     const params = new URLSearchParams({
-        
+        clientId: process.env.AURINKO_CLIENT_ID as string,
+        serviceType,
+        scopes: 'Mail.'
     })
 }
