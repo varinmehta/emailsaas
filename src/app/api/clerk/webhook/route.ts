633206269af58b498d34f555643065f7390b7202
varinmehta <mehtavarin2004@gmail.com>
2024-11-18 04:56:38 +0000

    11/18/2024, 10:26 AM

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -6,5 +6,16 @@ export const POST = async (req: Request) => {
     const lastName = data.last_name
     const imageUrl = data.image_url
     const id = data.id
+
+    // await db.user.create({
+    //     data: {
+    //         id: id,
+    //         emailAddress: emailAddress,
+    //         firstName: firstName,
+    //         lastName: lastName,
+    //         imageUrl: imageUrl,
+    //     }
+    // })
+    console.log('user created')
     return new Response('Webhook received', { status: 200 })
 }
