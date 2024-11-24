8157c122ebd1159649ab42a5796c6fa8a4444085
varinmehta <mehtavarin2004@gmail.com>
2024-11-24 20:46:29 +0530

    commit

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,9 +1,18 @@
+import { exchangeCodeForAccessToken } from "@/lib/aurinko"
 import { auth } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 // /api/aurinko/callback
-export const GET = async (req: Request) => {
+export const GET = async (req: NextRequest) => {
     const { userId } = await auth
+    if (!userId) return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+    const params = req.nextUrl.searchParams
+    const status = params.get('status')
+    if (status != 'success') return NextResponse.json({ message: 'Failed to link account' }, { status: 400 })
+    const code = params.get('code')
+    if (!code) return NextResponse.json({ message: 'No code provided' }, { status: 400 })
+    const token = await exchangeCodeForAccessToken(code)
+    if (!token) return NextResponse.json({ message: 'Failed to exchange code for token' }, { status: 401 })
     console.log('user id is', userId)
     return NextResponse.json({message: "Hello World!"})
 }
--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -1,5 +1,7 @@
 "use server"
 import { auth } from "@clerk/nextjs/server"
+import axios from 'axios'
+import { error } from "console"
 export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => {
     const { userId } = await auth()
     if (!userId) throw new Error("Unauthorized")
@@ -12,4 +14,26 @@ export const getAurinkoAuthUrl = async (serviceType: 'Google' | 'Office365') => 
     })
 
     return `https://api.aurinko.io/v1/auth/authorize?${params.toString()}`
+}
+
+export const exchangeCodeForAccessToken = async (code: string) => {
+    try {
+        const response = await axios.post(`https://api.aurinko.io/v1/auth/token/${code}`, {
+            auth: {
+                username: process.env.AURINKO_CLIENT_ID as string,
+                password: process.env.AURINKO_CLIENT_SECRET as string
+            }
+        })
+        return response.data as {
+            accountId: number,
+            accessToken: string,
+            userId: string,
+            userSession: string
+        }
+    } catch (err) {
+        if (axios.isAxiosError(error)) {
+            console.error(error.response?.data);
+        }
+        console.error(error)
+    }
 }
